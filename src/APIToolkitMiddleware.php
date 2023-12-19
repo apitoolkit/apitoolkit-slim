acbab8e33f5ca2ea3842e3e65f57ec72bab2389b
dawkaka <yousiph77@gmail.com>
2023-12-19 12:12:55 +0000

    support for outgoing requests

--- src/APIToolkitMiddleware.php
+++ src/APIToolkitMiddleware.php
@@ -16,6 +16,7 @@ use Monolog\Handler\StreamHandler;
 use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Middleware as GuzzleMiddleware;
+use GuzzleHttp\Psr7\Response as GuzzleResponse;
 use Ramsey\Uuid\Uuid;
 
 
@@ -121,11 +122,9 @@ class APIToolkitMiddleware
 
   public static function observeGuzzle($request)
   {
-
     $handlerStack = HandlerStack::create();
     $request_info = [];
     $handlerStack->push(GuzzleMiddleware::mapRequest(function ($request) use (&$request_info) {
-      echo "Before Request: " . $request->getMethod() . ' ' . $request->getUri() . PHP_EOL;
       $request_info = [
         "start_time" => hrtime(true),
         "method" => $request->getMethod(),
@@ -133,7 +132,7 @@ class APIToolkitMiddleware
         "headers" => $request->getHeaders(),
         "body" => $request->getBody()->getContents(),
         "query" => $request->getUri()->getQuery(),
-        "host" => $request->getHost(),
+        "host" => $request->getUri()->getHost(),
       ];
       return $request;
     }));
@@ -143,6 +142,7 @@ class APIToolkitMiddleware
       $client = $apitoolkit['client'];
       $msg_id = $apitoolkit['msg_id'];
       $projectId = $apitoolkit['project_id'];
+      $respBody = $response->getBody()->getContents();
       $payload = [
         'duration' => round(hrtime(true) - $request_info["start_time"]),
         'host' => $request_info["host"],
@@ -154,10 +154,10 @@ class APIToolkitMiddleware
         'path_params' =>  [],
         'raw_url' => $request_info["raw_url"],
         'referer' => "",
-        'request_headers' => $this->redactHeaderFields($this->redactHeaders, $request_info["headers"]),
-        'response_headers' => $this->redactHeaderFields($this->redactHeaders, $response->getHeaders()),
-        'request_body' => base64_encode($this->redactJSONFields($this->redactRequestBody, $request_info["body"])),
-        'response_body' => base64_encode($this->redactJSONFields($this->redactResponseBody, $response->getBody()->getContents())),
+        'request_headers' => self::redactHeaderFields([], $request_info["headers"]),
+        'response_headers' => self::redactHeaderFields([], $response->getHeaders()),
+        'request_body' => base64_encode(self::redactJSONFields([], $request_info["body"])),
+        'response_body' => base64_encode(self::redactJSONFields([], $respBody)),
         'errors' => [],
         'sdk_type' => 'GuzzleOutgoing',
         'parent_id' => $msg_id,
@@ -165,9 +165,17 @@ class APIToolkitMiddleware
         'timestamp' => (new \DateTime())->format('c'),
         'url_path' => "",
       ];
-      error_log(json_encode($payload));
       $client->publishMessage($payload);
-      return $response;
+      $newBodyStream = \GuzzleHttp\Psr7\Utils::streamFor($respBody);
+
+      $newResponse = new GuzzleResponse(
+        $response->getStatusCode(),
+        $response->getHeaders(),
+        $newBodyStream,
+        $response->getProtocolVersion(),
+        $response->getReasonPhrase()
+      );
+      return $newResponse;
     }));
 
     $client = new Client(['handler' => $handlerStack]);
@@ -184,13 +192,10 @@ class APIToolkitMiddleware
     $request = $request->withAttribute('apitoolkitData', $data);
     $data = $request->getAttribute('apitoolkitData');
     $errors = $data["errors"];
-    error_log(json_encode($errors));
-    return $request;
   }
 
   public function publishMessage($payload)
   {
-
     $data = json_encode($payload, JSON_UNESCAPED_SLASHES);
     if ($this->debug) {
       $this->logger->debug("APIToolkit: payload" . $data);
@@ -221,7 +226,6 @@ class APIToolkitMiddleware
     $pattern = $route->getPattern();
     $pathWithQuery = $path . ($query ? '?' . $query : '');
     $errors = $request->getAttribute('apitoolkitData')['errors'];
-    $this->logger->debug("APIToolkit: errors", $errors);
     return [
       'duration' => round(hrtime(true) - $startTime),
       'host' => $request->getUri()->getAuthority(),
@@ -240,6 +244,8 @@ class APIToolkitMiddleware
       'errors' => $errors,
       'sdk_type' => 'PhpSlim',
       'msg_id' => $msg_id,
+      'tags' => $this->tags,
+      'service_version' => $this->serviceVersion,
       'parent_id' => null,
       'status_code' => $response->getStatusCode(),
       'timestamp' => (new \DateTime())->format('c'),
